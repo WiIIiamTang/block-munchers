30e068012b7bcb426bf185b9c5e39d5c65da3edb
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2020-09-19 15:44:40 -0400

    new blocks and fix host

--- host.py
+++ host.py
@@ -1,13 +1,10 @@
-from server.game_server import Server
+from server.game_server import GServer
 import sys
 
 if len(sys.argv) == 2:
-    server = Server('', int(sys.argv[1]))
-elif len(sys.argv) == 3:
-    server = Server(sys.argv[1], int(sys.argv[2]))
+    server = GServer('', int(sys.argv[1]))
 else:
-    ip = input('ip address:')
     port = input('port:')
-    server = Server(ip='' if not ip else ip, port=int(port))
+    server = GServer(ip='', port=int(port))
 
 server.handle_connections()
